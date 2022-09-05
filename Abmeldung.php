9c9c8b39bf6bde112ff6f4e43938d7517a70cead
RachelOng03 <110903044+RachelOng03@users.noreply.github.com>
2022-09-05 16:30:38 +0200

    Add files via upload

---
+++ Abmeldung.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Laden der benötigten Style Sheets / Schriftarten -->
+  <link rel="stylesheet" href="Startseite.css">
+  <link rel="stylesheet" href="Abmeldung.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Aboreto&family=Roboto:wght@100&display=swap" rel="stylesheet">
+  <meta charset="utf-8">
+
+</head>
+
+<body>
+
+<!-- Header -->
+  <div id="navbar">
+    <a href="Startseite.html" id="logo">Out & About</a>
+    <div id="navbar-right">
+      <a href="Produktuebersichtseite.php">Cars</a>
+      <a href="Cities.html">Cities</a>
+      <a href="Meine_Buchungen.html">Meine Buchungen</a>
+      <a href="login" class="icon" ><img src="Bilder/Homepage/Icon-Login.png"></a>
+    </div>
+  </div>
+
+<!-- Beginn Content -->
+
+<!-- Hilsboxen um Bild und Content nebeneinander zu legen -->
+  <div class = "hilfsbox">
+    <div class = "hilfsbox2">
+
+    <div class = "halbiert"> <img src="Bilder/Abmeldung/abmeldungsbild.jpg" style = " border-bottom-left-radius: 30px">
+    </div>
+    <div class = "halbiert" style = "background-color: #214429; border-top-right-radius: 30px">
+
+<!-- Abmeldungs Icon & Text -->
+    <div class = "content_box">
+
+      <div class = "abmeldungs_icon"> <img src="Bilder/Abmeldung/icon_abmeldung.png"></div>
+
+      <div class = "abmeldungs_text">
+      <h1> Thanks for driving with Out & About. We hope to see you soon. </h1>
+      </div>
+
+      <div class = "abmeldungs_text">
+      <p> We hope you will drive with us again soon.</p>
+      </div>
+
+      <div class="linie"></div>
+      <br><br>
+
+      <a href= "Startseite.html"> <div class="weiße_button" style="border-top-right-radius: 30px">
+      ZURÜCK ZUR STARTSEITE
+      </div> </a>
+
+      <br>
+
+      <a href= "Login.php"> <div class="weiße_button">ZURÜCK ZUM LOGIN
+      </div> </a>
+
+      <br>
+
+      <a href= "Produktuebersichtseite.php"> <div class="weiße_button" style="border-bottom-left-radius: 30px">
+      ANGEBOTE DURCHSUCHEN
+      </div> </a>
+
+    </div>
+
+
+    </div>
+  </div>
+  </div>
+
+<!-- Footer -->
+<div class="footer">
+    <div class="oft_besucht"><h4>Oft Besucht</h4></div>
+      <div class="footer_startseite">
+      <a href="Startseite.html">Startseite</a></div>
+      <div class="footer_cars">
+      <a href="Produktuebersichtseite.html">Cars</a></div>
+      <div class="footer_cities">
+      <a href="Cities.html">Cities</a>
+    </div>
+  <div class="legal"><h4>Legal</h4>
+    <div class="footer_datenschutz">
+      <a href="#">Datenschutz</a></div>
+    <div class="footer_agb">
+      <a href="#">AGB & Rechtliches</a></div>
+    <div class="footer_impressum">
+      <a href="#">Impressum</a></div>
+  </div>
+  <div class="about"><h4>About us</h4></div>
+    <div class="footer_team">
+      <a href="Teamseite.php">Das Team</a></div>
+  <div class="copyright">Copyright 2022</div>
+    <a href="haha"><img src="Bilder/Footer/instagram.png" class="instagram"></a>
+    <a href="haha"><img src="Bilder/Footer/facebook.png" class="facebook"></a>
+    <a href="haha"><img src="Bilder/Footer/twitter.png" class="twitter"></a>
+</div>
+
+</body>
+</html>
